274f2a670d5fceff92f5e8ce4143b293da0407c4
im-ashar <muhammadashar.401@gmail.com>
2023-09-14 11:45:55 +0500

    Add project files.

---
+++ TodoAppMaui.Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TodoAppMaui.Shared;
+
+namespace TodoAppMaui.Database
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoAppMaui;");
+        }
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
